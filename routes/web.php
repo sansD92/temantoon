16309d012a3548740fd9e008c6b7d70c76c41de1
sansD92 <adjiedwisandy@gmail.com>
2023-12-10 02:15:55 +0700

    feat: slicing login & signup

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::redirect('/', 'prototype/login');
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -25,9 +26,17 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    Route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login'); 
+    Route::get('/register', function () {
+        return Inertia::render('Prototype/Register');
+    })->name('register'); 
+    Route::get('/dashboard', function () {
+        return Inertia::render('Prototype/Dashboard');
+    })->name('dashboard'); 
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
