7e068c634ebb7133cbb4b89d323396ecc9ceb270
sansD92 <adjiedwisandy@gmail.com>
2023-12-11 01:39:02 +0700

    feat: slicing dashboard card

--- resources/js/Layouts/Authenticated/Topbar.jsx
+++ resources/js/Layouts/Authenticated/Topbar.jsx
@@ -1,4 +1,17 @@
+import { useRef, useState } from "react";
+
 export default function Topbar(){
+    const [dropdownOpen, setdropdownOpen] = useState(true)
+    const dropdownTarget = useRef();
+
+    const trigerDropdown = () =>{
+        if (dropdownOpen) {
+            dropdownTarget.current.classList.remove('hidden')
+        } else {
+            dropdownTarget.current.classList.add('hidden');
+        }
+        setdropdownOpen(!dropdownOpen);
+    }
     return (
         <div className="flex justify-between items-center">
         <input type="text" className="top-search" placeholder="Search movie, cast, genre"
@@ -10,10 +23,10 @@ export default function Topbar(){
                 <a href="#!"
                     className="outline outline-2 outline-gray-2 p-[5px] rounded-full w-[60px] dropdown-button"
                     data-target="#dropdown-button">
-                    <img src="/images/avatar.png" className="rounded-full object-cover w-full" alt="" />
+                    <img src="/images/avatar.png" className="rounded-full object-cover w-full" alt="" onClick={trigerDropdown}/>
                 </a>
                 <div className="bg-white rounded-2xl text-black font-medium flex flex-col gap-1 absolute z-[999] right-0 top-[80px] min-w-[180px] hidden overflow-hidden"
-                    id="dropdown-target">
+                    ref={dropdownTarget}>
                     <a href="#!" className="transition-all hover:bg-sky-100 p-4">Dashboard</a>
                     <a href="#!" className="transition-all hover:bg-sky-100 p-4">Settings</a>
                     <a href="sign_in.html" className="transition-all hover:bg-sky-100 p-4">Sign Out</a>
--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,5 +1,60 @@
 import Authenticated from "@/Layouts/Authenticated/Index"
-
+import Flickity from "react-flickity-component";
+import { Head } from "@inertiajs/react";
+import FeaturedMovie from "@/Components/FeaturedMovies";
+import MovieCard from "@/Components/MovieCard";
 export default function Dashboard(){
-    return <Authenticated />
+    const flickityOptions ={
+        "cellAlign": "left",
+        "contain": true,
+        "groupCells": 1,
+        "wrapAround": false,
+        "pageDots": false,
+        "prevNextButtons": false,
+        "draggable": ">1",
+    };
+    return <Authenticated>
+        <Head>
+        <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css" />
+        </Head>
+         <div>
+                    <div className="font-semibold text-[22px] text-black mb-4">Featured Movies</div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                         {/* <!-- Movie Thumbnail --> */}
+                        {[1,2,3,4].map((i) => (
+                            <FeaturedMovie 
+                            key={i}
+                            slug="Punk-in-love"
+                            name={`Punk In Love ${i}`}
+                            category="Comedy"
+                            thumbnail="http://picsum.photos/id/1/300/300"
+                            rating={i + 1}
+                             />
+                           
+                        ))}
+                        
+                       
+
+                       
+                    </Flickity>
+                </div>
+
+                {/* <!-- Browse --> */}
+                <div className="mt-[50px]">
+                    <div className="font-semibold text-[22px] text-black mb-4">Browse</div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {/* <!-- Movies 1 --> */}
+                       {[1,2,3,4,5,6].map((i) => (
+                       <MovieCard 
+                       key={i}
+                       slug="Spiderman-the-movie"
+                       name={`Spiderman The Movie ${i}`}
+                       category="Action"
+                       thumbnail="https://picsum.photos/id/918/200/300"
+                       />
+                       ))}
+                       
+                    </Flickity>
+                </div>
+    </Authenticated>
 }
